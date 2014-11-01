17a4e21796c53e072fab92539a9ccdcbe6f9ae7f
Collin Forrester <collin.forrester@gmail.com>
2014-11-01 16:01:13 -0500

    more progress on the slides. server resp updates to send only json

--- server/api/controllers/ContactController.js
+++ server/api/controllers/ContactController.js
@@ -6,6 +6,25 @@
  */
 
 module.exports = {
-	
+	sms: function(req, res) {
+		var id = req.params.all().id;
+
+		if(!id) {
+			return res.badRequest('Missing contact `id`');
+		}
+
+		sails.models.contact.findOne(id).exec(function(err, contact) {
+			if(err) {
+				return res.serverError(err);
+			}
+			if(!contact) {
+				return res.notFound('Contact with id `' + id + '` not found.');
+			}
+			if(!contact.phone) {
+				return res.badRequest('No phone number found for ' + contact.name);
+			}
+			return res.ok('SMS sent to ' + contact.name);
+		});
+	}
 };
 
