de9b5fb71497e52c9f4d0ee3abfc00df1ec2ca5a
Collin Forrester <collin.forrester@gmail.com>
2014-11-04 16:57:10 -0600

    fix failing tests for all labs

--- sample-project/test/spec/directives/favoritestate.js
+++ sample-project/test/spec/directives/favoritestate.js
@@ -11,10 +11,4 @@ describe('Directive: favoriteState', function () {
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
   }));
-
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<favorite-state></favorite-state>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the favoriteState directive');
-  }));
 });
