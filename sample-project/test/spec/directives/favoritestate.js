b60d0f50946a0ce49cce7f5ccedb9f84db73523a
Collin Forrester <collin.forrester@gmail.com>
2014-11-04 17:06:23 -0600

    fix failing jshint error for all labs

--- sample-project/test/spec/directives/favoritestate.js
+++ sample-project/test/spec/directives/favoritestate.js
@@ -5,7 +5,7 @@ describe('Directive: favoriteState', function () {
   // load the directive's module
   beforeEach(module('phonebookApp'));
 
-  var element,
+  var /*element,*/
     scope;
 
   beforeEach(inject(function ($rootScope) {
